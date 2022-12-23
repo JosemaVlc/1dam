38be195a210deb1bb95755d9a840983cfab06146
Josema Moreno <j.moreno.bolivar@gmail.com>
2022-12-23 06:51:39 +0100

    mejoro LM y empiezo con crear_tauler

--- PROG/hundir_la_flota/src/hundir_la_flota/Hundir_la_flota.java
+++ PROG/hundir_la_flota/src/hundir_la_flota/Hundir_la_flota.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package hundir_la_flota;
 import java.util.Scanner;
 /**
@@ -29,6 +25,20 @@ public class Hundir_la_flota {
         }
         return a;
     }
+    public static void crear_tauler(){   
+        
+    }
+    public static void inserir_barcos(int a){   
+        
+    }    
+    public static void jugar_partida(int a){   
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("""
+                           ===== NOVA PARTIDA =====
+                           ESCOMENÇEM...""");
+        crear_tauler();
+        inserir_barcos(a);
+    }
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.print("""
@@ -39,10 +49,9 @@ public class Hundir_la_flota {
                          2.Mitja: 2 llanxes, 1 vaixell, 1 cuirassat i 1 portaavions (30 trets).
                          3.Dificil: 1 llanxa y 1 vaixell (10 trets).
                          Quin nivell tries?: """);
-        int opc=entrada.nextInt();
-        int lvl = demana_dades_entre_max_i_min(opc);
-        System.out.print(lvl);
-        
+        int lvl=entrada.nextInt();
+        int opc = demana_dades_entre_max_i_min(lvl);
+        jugar_partida(opc);      
     }
     
 }
