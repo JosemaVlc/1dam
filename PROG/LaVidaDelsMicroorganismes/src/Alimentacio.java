0b7804fc218655c163e95f3919cd821054205d26
josema <j.moreno.bolivar@gmail.com>
2023-03-20 17:59:18 +0100

    trabajo

---
+++ PROG/LaVidaDelsMicroorganismes/src/Alimentacio.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jmore
+ */
+
+enum aliment{
+    Tot, Ameba, Bacteri, Alga, Nutrient
+}
+public class Alimentacio {
+    final private int pesAmeba = 20;
+    final private int pesBacteri = 10;
+    final private int pesAlga = 3;
+    final private int pesNutrients = 5;
+    
+    public void menjar(String qui, int com){
+        
+    }
+}
