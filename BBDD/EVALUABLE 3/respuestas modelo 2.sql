7af0a4c6768ab20dbbb261af03c1cfb73088d2a3
josema <j.moreno.bolivar@gmail.com>
2023-03-30 17:52:18 +0200

    trabajo

--- BBDD/EVALUABLE 3/respuestas modelo 2.sql
+++ BBDD/EVALUABLE 3/respuestas modelo 2.sql
@@ -2,10 +2,12 @@ USE acadjedidb;
 
 /**
 SCRIPT 1B: ACADEMIAS Y CURSOS POR COSTES ASOCIADOS (2 PUNTOS)
+
 Crea una función para obtener, dado el código de academia y código de curso, el número de costes
 asociados y una segunda función para obtener, también dado un código de academia y código de
 curso, los euros totales que ha costado. Usando las dos funciones, lista los 2 cursos que más costes
 asociados tienen y otro listado para los 2 cursos que más dinero han costado. 
+
 Se pide:
 • Crea la función fNumcostesAcadCurso
 • Crea la función fTotalCostesAcadCurso
@@ -42,16 +44,23 @@ BEGIN
 END$$
 DELIMITER ;
 
-SELECT distinct codcurso, codacad, fNumcostesAcadCurso(codcurso, codacad) AS Coste_Total FROM coste ORDER BY Coste_Total desc limit 2;
-SELECT distinct codcurso, codacad, fTotalCostesAcadCurso(codcurso, codacad) AS Coste_Total FROM coste ORDER BY Coste_Total desc limit 2;
+SELECT distinct codcurso, codacad, fNumcostesAcadCurso(codcurso, codacad) AS Coste_Total 
+FROM coste 
+ORDER BY Coste_Total DESC LIMIT 2;
+
+SELECT distinct codcurso, codacad, fTotalCostesAcadCurso(codcurso, codacad) AS Coste_Total 
+FROM coste 
+ORDER BY Coste_Total DESC LIMIT 2;
 
 /**
 SCRIPT 2B: LISTAR ALUMNOS POR LETRA Y TIPO (2 PUNTOS)
+
 Crea un procedimiento almacenado para obtener el nombre completo y la descripción del curso en
 que se encuentran matriculados los alumnos cuyo nombre comienza por la letra pasada como
 parámetro (de entrada) y son del tipo pasado como parámetro (de entrada) (P para Padawan y S
 para Senior) y devuelve el número de resultados en un tercer parámetro (de salida). Ordena los
 resultados por el nombre completo.
+
 Se pide:
 • Crea el procedimiento pListarAlumnos_porLetrayTipo
 • Muestra un mensaje de error si no se recibe una ‘P’ o una ‘S’ como 2º parámetro.
@@ -108,6 +117,7 @@ SELECT @resultados AS 'Numero de resultados';
 
 /**
 SCRIPT 3B: PÉRDIDA SEMÁNTICA EN ALUMNOS (T+D) (2 PUNTOS)
+
 Al traducir del diseño conceptual al modelo relacional, existía una pérdida semántica en las
 especializaciones que no eran Parcial+Solapada (PS), quedándose como una restricción de
 integridad que dijimos que “ya resolveríamos más adelante”.
@@ -118,6 +128,7 @@ Te proponemos crear los triggers necesarios para asegurar la especialización DI
 ALUMNOS en PADAWAN y SENIOR durante las inserciones, ignorando las actualizaciones y los
 borrados por su complejidad. (Los datos del script DDL pueden no cumplir estas restricciones, pero
 el trigger servirá a partir de ahora)
+
 Se pide:
 • Crea el trigger necesario para prevenir inserciones incorrectas en PADAWAN
 • Crea el trigger necesario para prevenir inserciones incorrectas en SENIOR
@@ -130,14 +141,24 @@ BEFORE INSERT ON padawan
 FOR EACH ROW
 BEGIN	
 	IF (
-		(SELECT s.idalumno 
+		(SELECT COUNT(s.idalumno) 
          FROM senior s 
-         WHERE NEW.idalumno = s.idalumno) > 0 )
+         WHERE NEW.idalumno = s.idalumno) > 0 
+		)
     THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar un Padawan que tenga una especialización Senior';
+	ELSEIF (
+		NEW.idalumno NOT IN (SELECT idalumno FROM alumno)
+		)
+	THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar un Padawan que no sea alumno';
 	END IF;
 END$$
 DELIMITER ;
+
+/** Prueba de insercion en la tabla PADAWAN **/
+INSERT INTO padawan VALUES('ALU006',current_date()); /** Alumno senior **/
+INSERT INTO padawan VALUES('ALU016',current_date()); /** No Alumno **/
 
 DROP TRIGGER IF EXISTS insercionDisjuntaSenior;
 DELIMITER $$
@@ -146,14 +167,106 @@ BEFORE INSERT ON senior
 FOR EACH ROW
 BEGIN	
 	IF (
-		(SELECT p.idalumno 
+		(SELECT COUNT(p.idalumno)
          FROM padawan p 
-         WHERE NEW.idalumno = p.idalumno) > 0 )
+         WHERE NEW.idalumno = p.idalumno) > 0 
+		)
     THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar un Senior que tenga una especialización Padawan';
+	ELSEIF (
+		NEW.idalumno NOT IN (SELECT idalumno FROM alumno)
+		)
+	THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede insertar un Senior que no sea alumno';
 	END IF;
 END$$
 DELIMITER ;
-    
 
+/** Prueba de insercion en la tabla SENIOR **/
+INSERT INTO senior VALUES('ALU009','sable láser',current_date()); /** Alumno padawan **/
+INSERT INTO senior VALUES('ALU016','sable láser',current_date()); /** No Alumno **/
 
+/**
+SCRIPT 4B: TRIGGERS PARA LAS PARTICIPACIONES 1:N (2 PUNTOS)
+
+Al traducir del diseño conceptual al modelo relacional, existía una pérdida semántica en las
+participaciones 1:N, quedándose como una restricción de integridad que dijimos que “ya
+resolveríamos más adelante”.
+Pues bien, mediante triggers podemos traducir cualquier participación 1:N y superar así esa pérdida
+semántica de manera sencilla.
+Te proponemos crear los triggers necesarios para asegurar la participación 1:N de la relación entre
+CURSO y ALUMNO, ignorando las inserciones porque no afectan a la 1:N.
+
+Se pide:
+• Crea el trigger necesario para prevenir borrados de ALUMNO que rompan la participación
+1:N en la relación de CURSO con ALUMNO.
+• Crea el trigger necesario para prevenir actualizaciones del campo CODCURSO de la tabla
+ALUMNO que rompan la participación 1:N en la relación de CURSO con ALUMNO.
+**/
+
+DROP TRIGGER IF EXISTS borradoAlumno;
+DELIMITER $$
+CREATE TRIGGER borradoAlumno	
+BEFORE DELETE ON Alumno
+FOR EACH ROW
+BEGIN
+    IF ((SELECT COUNT(idalumno) 
+		FROM Alumno 
+        WHERE OLD.codacad = codacad 
+        AND OLD.codcurso = codcurso) <= 1)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede borrar el ultimo alumno que queda en el curso';
+	END IF;
+END$$
+DELIMITER ;
+
+/** Prueba de borrado del ultimo alumno asociado a un curso **/
+DELETE FROM Alumno WHERE idalumno = 'ALU003';
+
+DROP TRIGGER IF EXISTS actualizacionAlumno;
+DELIMITER $$
+CREATE TRIGGER actualizacionAlumno	
+BEFORE UPDATE ON alumno
+FOR EACH ROW
+BEGIN
+    IF 
+		(((SELECT COUNT(idalumno) 
+		   FROM Alumno 
+           WHERE OLD.codacad = codacad 
+           AND OLD.codcurso = codcurso) <= 1)
+		AND NEW.codcurso <> OLD.codcurso
+        )
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No se puede actualizar el campo CODCURSO del ultimo alumno que queda en el curso';
+	END IF;
+END$$
+DELIMITER ;
+
+/** Prueba de borrado del ultimo alumno asociado a un curso **/
+UPDATE Alumno SET codcurso = 8050 WHERE idalumno = 'ALU003';
+
+/**
+SCRIPT 5B: LÍNEAS DE COSTES CONSECUTIVAS (2 PUNTOS)
+
+Crea los triggers necesarios para asegurar que los números de línea de un mismo coste son
+consecutivos cuando se inserta una nueva línea de coste de un curso. Para hacerlo más sencillo,
+ignora los borrados, gestiona las inserciones y prohíbe las actualizaciones del campo NUMLINEA en
+esa tabla.
+
+Se pide:
+• Crea el trigger tAntesActualizarLineasCoste
+	◦   Muestra un mensaje de error cuando se intenta cambiar el campo LINEA
+• Crea el trigger tAntesInsertarLineasCoste
+	◦   El campo NUMLINEA debe ser >0 y siempre consecutivo para un mismo COSTE, de
+		manera que, si por ejemplo insertamos la línea 6 del coste del curso ‘6050’ y academia
+		‘GLEE’, debe existir antes el 5. Si no existe esa línea 5, debe cancelarse la operación con
+		un mensaje de error.
+**/
+select * from coste;
+DROP TRIGGER IF EXISTS tAntesActualizarLineasCoste;
+DELIMITER $$
+CREATE TRIGGER tAntesActualizarLineasCoste	
+BEFORE UPDATE ON coste
+FOR EACH ROW
+BEGIN
+	select count(linea) from coste where codcurso = new.codcurso and codacad = new.codacad;
